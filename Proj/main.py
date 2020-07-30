337d23e3ab553466ba335c90e3026719b16f4cda
Dickensia <33457436+Dickensia@users.noreply.github.com>
2020-07-30 12:34:55 +0800

    init

---
+++ Proj/main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 23 10:24:32 2020
+
+@author: David
+"""
+
+root='C:\\Users\\David\\Desktop\\demo\\'
+from demo.web2 import web2
+from demo.web3 import web3
+
+
+def main(root):
+    result2 = web2.web2(root)
+    result = web3.web3(result2,root)
+    return result
+    
+
+
+
+
+if __name__ == '__main__':
+    main(root)
